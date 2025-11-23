6b7da5776db0ace377c6ef00d91e35ed25dbc7c0
harshaljambhale17 <harshaljambhale17@gmail.com>
2025-11-23 15:03:10 +0530

    Add check of backend url

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const baseURL = import.meta.env.VITE_API_BASE_URL;
+console.log("API Base URL:", baseURL);
 
 export const httpClient = axios.create({
     baseURL: baseURL
